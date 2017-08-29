fb605ac82aa6c0f00f1bc0a389310a0e0ed8422b
mfentane <myriam.fentanes@gmail.com>
2017-08-29 16:28:52 -0500

    Defining the APICastBean configuration on the the camel-config.xml

--- src/main/java/org/apache/camel/example/cxf/proxy/APICastBean.java
+++ src/main/java/org/apache/camel/example/cxf/proxy/APICastBean.java
@@ -9,6 +9,12 @@ import threescale.v3.api.impl.ServiceApiDriver;
 public class APICastBean {
 
 	ServiceApi serviceApi = ServiceApiDriver.createApi();
+	private String appID;
+	private String appKey;
+	private String serviceToken;
+	private String serviceID;
+	private String error;
+	
 	public void process()
 	{
 		// When connecting to an on-premise instance of the 3scale platform,
@@ -20,13 +26,11 @@ public class APICastBean {
 
 		//TODO Change with the 3Scale account information of your account.
 		ParameterMap params = new ParameterMap(); // the parameters of your call
-		params.add("app_id", "75c223c8"); // Add app_id of your application
+		params.add("app_id",appID); // Add app_id of your application
 												// for authorization
-		params.add("app_key", "df292e8ba1cb94012579d6fc34857348"); // Add app_key of your
+		params.add("app_key", appKey); // Add app_key of your
 												// application for authorization
-		String serviceToken = "da523f1bcd613d64f7750fdabca3676852085a3ebf557be6345c4178824cb7d5"; // Your 3scale service token
-		String serviceId = "2555417746568"; // The service id of your application
-
+	
 		ParameterMap usage = new ParameterMap(); // Add a metric to the call
 		usage.add("hits", "1");
 		params.add("usage", usage); // metrics belong inside the usage parameter
@@ -34,7 +38,7 @@ public class APICastBean {
 		AuthorizeResponse response = null;
 		// the 'preferred way' of calling the backend: authrep
 		try {
-			response = serviceApi.authrep(serviceToken, serviceId, params);
+			response = serviceApi.authrep(serviceToken, serviceID, params);
 			System.out.println("AuthRep on App Id Success: " + response.success());
 			if (response.success() == true) {
 				// your api access got authorized and the traffic added to
@@ -49,5 +53,53 @@ public class APICastBean {
 		} catch (ServerError serverError) {
 			serverError.printStackTrace();
 		}
+	}
+
+	public ServiceApi getServiceApi() {
+		return serviceApi;
+	}
+
+	public void setServiceApi(ServiceApi serviceApi) {
+		this.serviceApi = serviceApi;
+	}
+
+	public String getAppID() {
+		return appID;
+	}
+
+	public void setAppID(String appID) {
+		this.appID = appID;
+	}
+
+	public String getAppKey() {
+		return appKey;
+	}
+
+	public void setAppKey(String appKey) {
+		this.appKey = appKey;
+	}
+
+	public String getServiceToken() {
+		return serviceToken;
+	}
+
+	public void setServiceToken(String serviceToken) {
+		this.serviceToken = serviceToken;
+	}
+
+	public String getServiceID() {
+		return serviceID;
+	}
+
+	public void setServiceID(String serviceID) {
+		this.serviceID = serviceID;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
 	}
 }
