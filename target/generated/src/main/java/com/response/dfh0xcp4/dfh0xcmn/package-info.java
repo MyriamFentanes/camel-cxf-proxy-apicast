83f46f40e2baeed1f471a1a9ac6c4203d4ab8776
mfentane <myriam.fentanes@gmail.com>
2017-09-11 08:59:00 -0400

    added the certificates interceptor

--- target/generated/src/main/java/com/response/dfh0xcp4/dfh0xcmn/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.DFH0XCMN.DFH0XCP4.Response.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.response.dfh0xcp4.dfh0xcmn;
